6bcf82a707f655a43d680c0003ea63fc8c0e9c48
Christian Nagel <christian@christiannagel.com>
2016-09-18 19:38:26 +0200

    logging samples

---
+++ DotnetCore/ModernPatterns/Logging/LoggingSample/src/LoggingSample/LoggerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingSample
+{
+    public static class LoggerExtensions
+    {
+        private static Action<ILogger, DateTimeOffset, Exception> _programStarting;
+        static LoggerExtensions()
+        {
+            _programStarting = LoggerMessage.Define<DateTimeOffset>(LogLevel.Information, 1, "Starting at '{StartTime:t}'");
+        }
+        public static void ProgramStarting(this ILogger logger, DateTimeOffset startTime, Exception exception = null)
+        {
+            _programStarting(logger, startTime, exception);
+        }
+    }
+}
---
+++ DotnetCore/ModernPatterns/Logging/LoggingSample/src/LoggingSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingSample
+{
+    public class Program
+    {
+        private static ILogger<Program> _logger;
+
+        public static void Main(string[] args)
+        {
+            var settings = new FilterLoggerSettings();
+            settings.Add("Microsoft", LogLevel.Warning);
+
+            ILoggerFactory factory = new LoggerFactory()
+                .WithFilter(new FilterLoggerSettings
+                {
+                    { "Microsoft", LogLevel.Warning },
+                    { "LoggingSample.Program", LogLevel.Information }
+
+                });
+            factory.AddConsole();
+            factory.AddDebug();
+            _logger = factory.CreateLogger<Program>();
+
+            _logger.LogInformation(1, "started at '{StartTime:T}'", DateTimeOffset.Now);
+            _logger.ProgramStarting(DateTimeOffset.Now);
+
+        }
+
+    }
+}
