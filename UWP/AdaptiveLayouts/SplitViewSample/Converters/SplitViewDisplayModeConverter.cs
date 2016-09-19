ffe3f01c53b444925cb7ad14e3f7cd8f8195bff0
Christian Nagel <christian@christiannagel.com>
2016-09-19 14:38:28 +0200

    splitview sample

---
+++ UWP/AdaptiveLayouts/SplitViewSample/Converters/SplitViewDisplayModeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace SplitViewSample.Converters
+{
+    public class SplitViewDisplayModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return SplitViewDisplayMode.Overlay;
+
+            return Enum.Parse(typeof(SplitViewDisplayMode), value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
