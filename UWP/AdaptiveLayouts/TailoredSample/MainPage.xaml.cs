0dcbdc733d2da645e735addd4eb2a419b2c7ab1d
Christian Nagel <christian@christiannagel.com>
2016-09-19 16:49:40 +0200

    change to tailored sample

--- UWP/AdaptiveLayouts/TailoredSample/MainPage.xaml.cs
+++ UWP/AdaptiveLayouts/TailoredSample/MainPage.xaml.cs
@@ -1,12 +1,11 @@
-﻿using ChangingViews.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TailoredSample.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.System.Profile;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,7 +16,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace ChangingViews
+namespace TailoredSample
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -31,6 +30,6 @@ namespace ChangingViews
 
         }
 
-        public MainPageViewModel ViewModel { get;  }
+        public MainPageViewModel ViewModel { get; }
     }
 }
--- UWP/AdaptiveLayouts/TailoredSample/ViewModels/MainPageViewModel.cs
+++ UWP/AdaptiveLayouts/TailoredSample/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.System.Profile;
+﻿using Windows.System.Profile;
 
-namespace ChangingViews.ViewModels
+namespace TailoredSample.ViewModels
 {
     public class MainPageViewModel
     {
