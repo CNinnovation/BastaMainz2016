e5c871d95e44252be2d4a71c01b7f78b6bbf5173
Christian Nagel <christian@christiannagel.com>
2016-09-21 11:05:04 +0200

    AdaptiveGridView from UWP Toolkit

---
+++ UWP/AdaptiveLayouts/AdaptiveGridViewWithUWPToolkit/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using AdaptiveGridViewWithUWPToolkit.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptiveGridViewWithUWPToolkit
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private ObservableCollection<SampleItem> _items = new ObservableCollection<SampleItem>();
+        public IEnumerable<SampleItem> Items => _items;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            IEnumerable<SampleItem> items =
+                Enumerable.Range(0, 30).Select(i =>
+                    new SampleItem { Text = (i + 1).ToString() });
+
+            foreach (var item in items)
+            {
+                _items.Add(item);
+            }
+        }
+    }
+}
---
+++ UWP/AdaptiveLayouts/AdaptiveGridViewWithUWPToolkit/Models/SampleItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace AdaptiveGridViewWithUWPToolkit.Models
+{
+    public class SampleItem
+    {
+        public string Text { get; set; }
+
+    }
+}
