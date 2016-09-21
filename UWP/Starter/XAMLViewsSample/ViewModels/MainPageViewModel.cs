f4c48a908dbd845c5eb06f313ede0dae8b557362
Christian Nagel <christian@christiannagel.com>
2016-09-21 15:41:46 +0200

    add starter project for XAML Views

---
+++ UWP/Starter/XAMLViewsSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+
+namespace XAMLViewsSample.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public string Hello => "Hello from the ViewModel";
+        public string DeviceFamily => AnalyticsInfo.VersionInfo.DeviceFamily;
+    }
+}
